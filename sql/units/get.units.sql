e67ac03918c7ac89fac058fba77ae3aaaf7383c8
Amin Azarbadegan <amin@azarbadegan.net>
2017-02-21 19:49:00 +0330

    filtering admin in the units/fixing typo

--- sql/units/get.units.sql
+++ sql/units/get.units.sql
@@ -1,1 +1,1 @@
-select uid, name, username, is_branch from units where is_branch = ${is_branch} where lower(username)<>'admin'
+select uid, name, username, is_branch from units where is_branch = ${is_branch} and lower(username)<>'admin'
