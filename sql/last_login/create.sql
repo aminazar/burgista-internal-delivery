a38b18b57ccb2a88ee632af700ea5cfd03c04560
305-2 <saresalehi@yaoo.com>
2017-02-27 11:23:15 +0330

    pass all specs sareh salehi & change serial type to integer in  sql  create last_login table

--- sql/last_login/create.sql
+++ sql/last_login/create.sql
@@ -1,5 +1,5 @@
 CREATE TABLE last_login(
     lid serial not null primary key,
-    login_uid serial not null references units(uid) on delete cascade,
+    login_uid integer not null references units(uid) on delete cascade,
     login_date_time date not null
 )
