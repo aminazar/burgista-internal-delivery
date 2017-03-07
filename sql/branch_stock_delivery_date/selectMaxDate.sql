11dd23f570584da5c98b7630d3df82fb5ff253f0
Amin Azarbadegan <amin@azarbadegan.net>
2017-03-07 12:24:54 +0330

    Reverting SQL for selecting

--- sql/branch_stock_delivery_date/selectMaxDate.sql
+++ sql/branch_stock_delivery_date/selectMaxDate.sql
@@ -7,21 +7,36 @@ select
     last_count
 from (
     select
+        max(counting_date) as counting_date,
         bsddid,
         pid,
-        products.code as product_code,
-        products.name as product_name,
-        counting_date,
+        product_code,
+        product_name,
         branch_id
-    from
-        products
-    left outer join
-        branch_stock_delivery_date
-    on
-        product_id = pid
-        and branch_id=${uid}
-        and counting_date <= ${date}
-        and (counting_date=${date} or product_count is null)
+    from (
+        select
+            bsddid,
+            pid,
+            products.code as product_code,
+            products.name as product_name,
+            counting_date,
+            branch_id
+        from
+            products
+        left outer join
+            branch_stock_delivery_date
+        on
+            product_id = pid
+            and branch_id=${uid}
+            and counting_date <= ${date}
+            and (counting_date=${date} or product_count is null)
+    ) as list
+    group by
+        bsddid,
+        pid,
+        product_code,
+        product_name,
+        branch_id
 ) as main_list
 left outer join (
     select
