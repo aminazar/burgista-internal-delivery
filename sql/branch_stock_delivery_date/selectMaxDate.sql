29645610095ab5667cce205297bce99432444a10
Amin Azarbadegan <amin@azarbadegan.net>
2017-03-07 12:22:20 +0330

    Fixing post API, adding put API for stock

--- sql/branch_stock_delivery_date/selectMaxDate.sql
+++ sql/branch_stock_delivery_date/selectMaxDate.sql
@@ -7,36 +7,21 @@ select
     last_count
 from (
     select
-        max(counting_date) as counting_date,
         bsddid,
         pid,
-        product_code,
-        product_name,
+        products.code as product_code,
+        products.name as product_name,
+        counting_date,
         branch_id
-    from (
-        select
-            bsddid,
-            pid,
-            products.code as product_code,
-            products.name as product_name,
-            counting_date,
-            branch_id
-        from
-            products
-        left outer join
-            branch_stock_delivery_date
-        on
-            product_id = pid
-            and branch_id=${uid}
-            and counting_date <= ${date}
-            and (counting_date=${date} or product_count is null)
-    ) as list
-    group by
-        bsddid,
-        pid,
-        product_code,
-        product_name,
-        branch_id
+    from
+        products
+    left outer join
+        branch_stock_delivery_date
+    on
+        product_id = pid
+        and branch_id=${uid}
+        and counting_date <= ${date}
+        and (counting_date=${date} or product_count is null)
 ) as main_list
 left outer join (
     select
