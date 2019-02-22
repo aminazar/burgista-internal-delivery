9061cc8acd5f09e09a1cef63761ea0625fdeaa1f
aminazar <amin@azarbadegan.net>
2019-02-22 23:14:47 +0330

    trying to improve queries for delivery/inventory

--- sql/branch_stock_delivery_date/getBranchDelivery.sql
+++ sql/branch_stock_delivery_date/getBranchDelivery.sql
@@ -4,18 +4,20 @@ from
     products
 left outer join (
     select
-        bsddid,
-        product_count,
-        submission_time,
-        is_delivery_finalised,
-        real_delivery,
-        last_count.product_id,
-        last_count.counting_date
+        s.bsddid,
+        s.product_count,
+        last_count.submission_time,
+        last_count.product_count as last_product_count,
+        s.is_delivery_finalised,
+        s.real_delivery,
+        s.product_id,
+        s.counting_date
     from
         branch_stock_delivery_date s
     join
     (
-        select
+        select s2.* from
+        (select
             product_id,
             branch_id,
             max(counting_date) as counting_date
@@ -23,13 +25,15 @@ left outer join (
             branch_stock_delivery_date
         where
             counting_date <= ${date}
+            and
+            submission_time is not null
         group by
             product_id,
-            branch_id
+            branch_id) s1
+            join branch_stock_delivery_date s2 on s1.product_id=s2.product_id and s1.branch_id = s2.branch_id and s1.counting_date = s2.counting_date
     ) last_count
     on
         last_count.product_id = s.product_id
-        and last_count.counting_date = s.counting_date
         and last_count.branch_id = s.branch_id
     where
         s.branch_id = ${uid}
@@ -37,16 +41,20 @@ left outer join (
         (
             s.counting_date = ${date}
             or s.submission_time = ${date}
-            or s.real_delivery + s.product_count < s.min_stock
-            or s.real_delivery is null
+            or (s.real_delivery + s.product_count < s.min_stock and s.counting_date + 3 > ${date})
+            or (s.real_delivery is null and s.counting_date + 3 > ${date})
         )
     ) aggreg
 on
     aggreg.product_id = products.pid
 left outer join
-    branch_stock_rules
+    branch_stock_rules bsr
 on
-    products.pid = branch_stock_rules.pid
-    and branch_stock_rules.uid = ${uid}
+    products.pid = bsr.pid
+    and bsr.uid = ${uid}
 where
     prep_unit_id = ${prep_uid}
+    and
+        (bsr.date_rule <> ''
+        or
+        (bsr.date_rule is null and products.default_date_rule <> ''))
--- sql/branch_stock_delivery_date/selectMaxDate.sql
+++ sql/branch_stock_delivery_date/selectMaxDate.sql
@@ -6,17 +6,19 @@ select
     counting_date,
     bsddid,
     last_count,
-    product_count
+    product_count,
+    default_date_rule
 from
     products
 left outer join
 (
     select
-        last_count.counting_date,
+        s.counting_date,
         bsddid,
         product_count,
         last_count.product_id,
-        submission_time as last_count
+        last_count.counting_date as last_count,
+        date_rule
     from
     (
         select
@@ -28,6 +30,7 @@ left outer join
         where
             counting_date <= ${date}
             and branch_id = ${uid}
+            and submission_time is not null
         group by
             branch_id,
             product_id
@@ -37,12 +40,24 @@ left outer join
     on
         s.product_id = last_count.product_id
         and s.branch_id = last_count.branch_id
-        and s.counting_date = last_count.counting_date
+        and s.counting_date = ${date}
+    left outer join
+        branch_stock_rules bsr
+    on
+        bsr.pid = s.product_id
+        and
+        bsr.uid = s.branch_id
     where
-        s.counting_date = ${date}
-        or s.product_count is null
-        or s.real_delivery + s.product_count < s.min_stock
-        or s.real_delivery is null
+            (s.counting_date = ${date}
+            or s.product_count is null
+            or s.real_delivery + s.product_count < s.min_stock
+            or s.real_delivery is null)
+        and
+            s.counting_date + 3 > ${date}
 ) last_count_extended
 on
     pid = last_count_extended.product_id
+where
+    last_count_extended.date_rule <> ''
+    or
+    (last_count_extended.date_rule is null and products.default_date_rule <> '')
