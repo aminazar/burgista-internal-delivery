3ae7f523da46f69766b9ebddfc2350f3403d59fa
aminazar <amin@azarbadegan.net>
2019-02-24 09:28:40 +0330

    config/test change

--- sql/products/getWithPrepUnitData.sql
+++ sql/products/getWithPrepUnitData.sql
@@ -1,4 +1,4 @@
 select products.* , units.is_kitchen as prep_is_kitchen, prices.price
 from products
-inner join units on products.prep_unit_id = units.uid
-left join prices on products.pid = prices.product_id where prices.valid_to IS NULL;
+join units on products.prep_unit_id = units.uid
+left outer join prices on products.pid = prices.product_id and prices.valid_to IS NULL;
