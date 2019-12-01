e26dfc5213b6dc7cb8cf6c5843bea01a95f26fb4
farhad <mafphp@gmail.com>
2019-12-01 04:08:38 +0000

    query getbranchdelivery and selectmaxdate, check date less than todayand unit model check date

--- sql/branch_stock_delivery_date/selectMaxDate.sql
+++ sql/branch_stock_delivery_date/selectMaxDate.sql
@@ -57,7 +57,7 @@ left outer join (
         or s.real_delivery + s.product_count < s.min_stock
         or s.real_delivery is null)
         and
-            (s.counting_date + 3 > ${date} or s.insert_time >= ${date})
+            (s.counting_date < ${date} and s.counting_date + 3 > ${date})
 		 ) last_count_extended on
 	products.pid = last_count_extended.product_id
 where
