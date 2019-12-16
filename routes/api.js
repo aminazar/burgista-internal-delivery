574c530f7b20987cf5c47cbd896dafadf65f026e
bentoak-systems <mafphp@gmail.com>
2019-12-16 10:33:50 +0330

    added some params into api afterlogin

--- routes/api.js
+++ routes/api.js
@@ -62,7 +62,7 @@ router.get('/logout', (req, res) => {
   req.logout();
   res.sendStatus(200)
 });
-router.get('/validUser', apiResponse('Unit', 'afterLogin', false, ['user.name', 'user.username', 'user.is_branch', 'user.uid','user.is_kitchen']));
+router.get('/validUser', apiResponse('Unit', 'afterLogin', false, ['user.name', 'user.username', 'user.is_branch', 'user.uid','user.is_kitchen', 'user.is_reporter']));
 
 //checks to be sure users are authenticated
 router.all("*", function(req, res, next){
